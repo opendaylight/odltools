2c06764113bc580137c239d51d4abf48eebe6bec
Sam Hague <shague@redhat.com>
2018-06-21 21:18:24 -0400

    pbr cleanup
    Change-Id: Idc622d3c3d765e3757f5083cabb9efdb9f00cb68
    Signed-off-by: Sam Hague <shague@redhat.com>

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
     setup_requires=['pbr'],
-    pbr=True)
+    pbr=True
+)
